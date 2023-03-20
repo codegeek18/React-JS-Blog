c65839d453fe209608639f32079ad892fc1500da
Dheeraj Sharma <dheerajsksd40@gmail.com>
2023-03-20 23:24:51 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,27 @@
+import Feed from "./Feed"
+import { useStoreState } from "easy-peasy";
+
+const Home = ({ isLoading, fetchError }) => {
+
+    const searchResults  = useStoreState((state) => state.searchResults);
+    
+    return (
+        <main className="Home">
+                                        {/* before using custom hooks */}
+            {/* {posts.length ? (
+                <Feed posts={posts} />
+            ) : (
+                <p style={{marginTop: "2rem"}} >
+                    No posts to display.
+                </p>
+            )} */}
+            {isLoading && <p className="statusMsg">Loading posts...</p>}
+            {!isLoading && fetchError && <p className="statusMsg" style={{color:"red"}}>{fetchError}</p>}
+            {!isLoading && !fetchError && (searchResults.length ? <Feed posts={
+                searchResults} /> : < p className="statusMsg">No posts to display.</p>
+            )}
+        </main>
+    )
+}
+
+export default Home
